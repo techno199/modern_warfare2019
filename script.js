33a2be97334b9c519fd87adb0317d2254bd68093
Alex <techno199@live.ru>
2021-09-20 18:22:22 +0300

    update

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
-(function() {
+(function () {
     const STATE_CHANGED_EVENT = 'STATE_CHANGED_EVENT';
 
+    const bg_audio = new Audio('./media/cod_soundtrack_1.mp3');
+    bg_audio.currentTime = 25.98;
+
+    const button_hover_audio = [
+        new Audio('./media/hover_1.mp3')
+    ];
+    const button_click_audio = [
+        new Audio('./media/click_1.mp3'),
+        new Audio('./media/click_2.mp3'),
+        new Audio('./media/click_3.mp3'),
+        new Audio('./media/click_4.mp3'),
+        new Audio('./media/click_5.mp3')
+    ];
+
+    let filter_button_click_audio = new Audio('./media/click_filter.mp3');
+    filter_button_click_audio.volume = 0.3;
+    filter_button_click_audio = [ filter_button_click_audio ];
+
     const config = {
-        background_volume: 0.1
+        background_volume: 0.1,
+        audio_presets: [
+            {
+                name: '.default-button-audio',
+                hover: button_hover_audio,
+                click: button_click_audio
+            },
+            {
+                name: '.default-button-hover-audio',
+                hover: button_hover_audio
+            },
+            {
+                name: '.default-button-click-audio',
+                click: button_click_audio
+            },
+            {
+                name: '.filter-button-click-audio',
+                click: filter_button_click_audio
+            }
+        ]
     }
 
     let state = {
@@ -16,22 +53,11 @@
         window.dispatchEvent(stateChangedEvent);
     }
 
-    const bg_audio = new Audio('./media/cod_soundtrack_1.mp3');
-
-    const button_hover_audio = [
-        new Audio('./media/hover_1.mp3')
-    ];
-    const button_click_audio = [
-        new Audio('./media/click_1.mp3'),
-        new Audio('./media/click_2.mp3'),
-        new Audio('./media/click_3.mp3')
-    ];
-
-    window.addEventListener('load', function() {
+    window.addEventListener('load', function () {
         const modal = document.querySelector('.launcher');
         const launcher_play = document.querySelector('.launcher__play');
 
-        launcher_play.addEventListener('click', function() {
+        launcher_play.addEventListener('click', function () {
             modal.classList.add('hidden');
 
             initBackgroundAudio();
@@ -41,7 +67,7 @@
         })
     })
 
-    window.addEventListener(STATE_CHANGED_EVENT, function(e) {
+    window.addEventListener(STATE_CHANGED_EVENT, function (e) {
         if (state.sound_muted) {
             bg_audio.volume = 0;
         } else {
@@ -49,13 +75,11 @@
         }
     })
 
-    window.addEventListener('keydown', function() {
+    window.addEventListener('keydown', function () {
         setState({ sound_muted: !state.sound_muted });
     })
 
     function initBackgroundAudio() {
-        bg_audio.currentTime = 24.5;
-
         function handleClick() {
             window.removeEventListener('click', handleClick);
             bg_audio.volume = 0;
@@ -66,11 +90,11 @@
             const interval_ticks = audio_transition_time / interval_time;
             const audio_transition_value_per_tick = config.background_volume / interval_ticks;
 
-            const interval_id = setInterval(function() {
+            const interval_id = setInterval(function () {
                 bg_audio.volume = Math.min(config.background_volume, bg_audio.volume + audio_transition_value_per_tick);
-                
+
                 const is_transition_ended = bg_audio.volume === config.background_volume;
-                
+
                 if (is_transition_ended) {
                     clearInterval(interval_id);
                 }
@@ -81,21 +105,29 @@
     }
 
     function initButtonsAudio() {
-        const buttons = document.querySelectorAll('.btn:not(.muted)');
+        for (let audio_preset of config.audio_presets) {
+            let buttons = document.querySelectorAll(audio_preset.name);
 
-        buttons.forEach(function(button) {
-            button.addEventListener('mouseenter', function() {
-                playRandomAudio(button_hover_audio);
-            });
+            buttons.forEach(function (button) {
+                if (audio_preset.hover) {
+                    button.addEventListener('mouseenter', function () {
+                        playRandomAudio(audio_preset.hover);
+                    });
+                }
+                
+                if (audio_preset.click) {
+                    button.addEventListener('click', function () {
+                        playRandomAudio(audio_preset.click);
+                    });
+                }
+                
+            })
+        }
 
-            button.addEventListener('click', function() {
-                playRandomAudio(button_click_audio);
-            });
-        })
     }
 
     function initFpsCounter() {
-        setInterval(function() {
+        setInterval(function () {
             const randomized_fps = Math.floor(Math.random() * 60 + 150);
 
             setState({ fps: randomized_fps });
@@ -105,8 +137,8 @@
     function subscribeStatsValueChange() {
         const fps_counter_elem = document.querySelector('.game-stats__fps .game-stats__value');
         const mute_sound_elem = document.querySelector('.game-stats__mute-sound .game-stats__value');
-    
-        window.addEventListener(STATE_CHANGED_EVENT, function() {
+
+        window.addEventListener(STATE_CHANGED_EVENT, function () {
             fps_counter_elem.innerHTML = state.fps;
             mute_sound_elem.innerHTML = state.sound_muted ? 'Enabled' : 'Disabled';
         })
